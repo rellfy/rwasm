a5fba65470b376e6fc4f3c70f2d1431ff257ad66
Lorenzo Fontoura <lorenzo@nioel.com>
2020-12-04 02:57:46 +1000

    split main.rs and wasm.rs; make wasm functions more compact/flexible

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,15 @@
-
-pub static mut BUFFER: [u8; 1_000] = [0; 1_000];
+pub mod wasm;
 
 #[no_mangle]
-fn handle_buffer(length: *const u8) {
+fn handle_data_upload_example(length: *const u8) {
     let length = length as usize;
 
     log(format!("Received byte array from JS with {} elements:", length).as_str());
     for i in 0..length {
         unsafe {
-            log(format!("buffer[{}]: {}", i, BUFFER[i]).as_str());
+            log(format!("buffer[{}]: {}", i, wasm::get_buffer()[i]).as_str());
         }
     }
-}
-
-#[no_mangle]
-fn get_buffer_pointer() -> *const u8 {
-    unsafe {
-        BUFFER.as_ptr()
-    }
-}
-
-#[no_mangle]
-extern "C" {
-    pub fn console_log(msg: *const u8, length: usize);
 }
 
 #[no_mangle]
@@ -31,9 +18,7 @@ fn multiply(x: f32, y: f32) -> f32 {
 }
 
 fn log(msg: &str) {
-    unsafe {
-        console_log(msg.as_ptr(), msg.len());
-    }
+    wasm::send_bytes("console_log", msg.as_bytes());
 }
 
 // main does not require #[no_mangle] to be called from WASM.
